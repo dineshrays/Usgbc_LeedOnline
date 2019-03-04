4cb172666ec2aee9e7843c3cb4639876d6f004c6
akhil <akhilgelli1141@gmail.com>
2019-03-04 11:20:07 +0530

    AK: Completed the E2E And reports

---
+++ Usgbc_LeedOnline/Test Suites/SmokeTest/TS_SmokeTest_E2E_LeedOnline.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_SmokeTest_E2E_LeedOnline</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e52e004f-92af-4593-9df8-30b338f15e49</testSuiteGuid>
+   <testCaseLink>
+      <guid>639ee860-e046-4826-aabe-ecd3d8762558</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SmokeTest/TC_Project_Registration</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9edd274f-e96f-4f89-946f-f3ad7ae05a4c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SmokeTest/TC_Campus_Registration</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e3c7edc3-fb2f-4a03-bee5-b55ab60749d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SmokeTest/TC_Project_Verification</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
