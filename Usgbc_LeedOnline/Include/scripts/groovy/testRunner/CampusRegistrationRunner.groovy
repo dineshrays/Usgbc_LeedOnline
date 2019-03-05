923a4afb265f0cbd0592978c5edc2103aadcd2d3
unknown <dinesh.rays@gmail.com>
2019-03-05 21:49:33 +0530

    SS:Changes made in CampusRegistrationRunner.groovy added the Jenkis Absolute url.

--- Usgbc_LeedOnline/Include/scripts/groovy/testRunner/CampusRegistrationRunner.groovy
+++ Usgbc_LeedOnline/Include/scripts/groovy/testRunner/CampusRegistrationRunner.groovy
@@ -81,13 +81,13 @@ plugin = ["pretty",
 	"json:G:\\Sabareesh\\Usgbc_GitProjects\\Usgbc_LeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.json"],
 tags=['@SmokeTest'] )*/
 
-
+//C:\Program Files (x86)\Jenkins\workspace\UsgbcLeedOnline\Usgbc_LeedOnline\Include\features
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Program Files (x86)\\Jenkins\\workspace\\Usgbc_LeedOnline\\Include\\features\\Usgbc_LeedCampusReg.feature",glue="",
+@CucumberOptions(features = "C:\\Program Files (x86)\\Jenkins\\workspace\\UsgbcLeedOnline\\Usgbc_LeedOnline\\Include\\features\\Usgbc_LeedCampusReg.feature",glue="",
 plugin = ["pretty",
-	"junit:C:\\Program Files (x86)\\Jenkins\\workspace\\Usgbc_LeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.xml",
-	"html:C:\\Program Files (x86)\\Jenkins\\workspace\\Usgbc_LeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.html",
-	"json:C:\\Program Files (x86)\\Jenkins\\workspace\\Usgbc_LeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.json"],
+	"junit:C:\\Program Files (x86)\\Jenkins\\workspace\\UsgbcLeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.xml",
+	"html:C:\\Program Files (x86)\\Jenkins\\workspace\\UsgbcLeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.html",
+	"json:C:\\Program Files (x86)\\Jenkins\\workspace\\UsgbcLeedOnline\\Usgbc_LeedOnline\\MyReports\\CampusRegistration.json"],
 tags=['@SmokeTest'] )
 
 
