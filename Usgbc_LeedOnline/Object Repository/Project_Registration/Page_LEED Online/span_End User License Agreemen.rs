4a8e4113400e70a184d9a30bafe0262a9eee8220
unknown <dinesh.rays@gmail.com>
2019-03-05 23:15:48 +0530

    SS:Changes made in Xpath of the element

--- Usgbc_LeedOnline/Object Repository/Project_Registration/Page_LEED Online/span_End User License Agreemen.rs
+++ Usgbc_LeedOnline/Object Repository/Project_Registration/Page_LEED Online/span_End User License Agreemen.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//fieldset[5]/label/span</value>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='End User License Agreement'])[1]/following::span[1]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
